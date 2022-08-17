ae104ae2d8517b5e2cb94faeac588d14f2ca2171
brightonchege <brightonspak@gmail.com>
2022-08-17 19:59:11 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity){
+    switch(activity){
+        default:
+            return "This Saturday, I want to roller-skate!"
+        case "bathe my dog":
+            return   "This Saturday, I want to bathe my dog!" 
+    }
+}
+
+
+
+function mondayWork(activity){
+    switch(activity){
+        default:
+            return "This Monday, I will go to the office."
+        case "work from home":
+            return   "This Monday, I will work from home." 
+    }
+
+}
+
+function wrapAdjective(adjective){
+ return function(string){
+    return `You are ${adjective}${string}${adjective}!`
+ }
+}
